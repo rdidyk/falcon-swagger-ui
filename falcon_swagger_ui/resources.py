5026cb30361d16bd3a5ca7519178bd7d40a71cfc
Alan Orth <alan.orth@gmail.com>
2021-11-10 11:23:44 +0200

    falcon_swagger_ui/resources.py: Falcon 3.0.0b1 fix (#21)
    Falcon 3.0.0b1 was released and resp.body is deprecated:
    
        falcon.util.deprecation.DeprecatedWarning: Call to deprecated property body. Please use text instead.
    
    See: https://falcon.readthedocs.io/en/3.0.0b1/changes/3.0.0.html

--- falcon_swagger_ui/resources.py
+++ falcon_swagger_ui/resources.py
@@ -58,7 +58,7 @@ class SwaggerUiResource(object):
 
     def on_get(self, req, resp):
         resp.content_type = 'text/html'
-        resp.body = self.templates.render('index.html', **self.context)
+        resp.text = self.templates.render('index.html', **self.context)
 
 
 def register_swaggerui_app(app, swagger_uri, api_url, page_title='Swagger UI', favicon_url=None, config=None, uri_prefix=""):
